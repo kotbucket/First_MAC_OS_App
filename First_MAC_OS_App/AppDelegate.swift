320073ccfc9f5cda7503503ee54c46a3b86ea7e2
Kevin OBrien <kevinobrien@kevins-mac-mini>
2018-04-10 13:48:23 -0400

    Initial Commit

---
+++ First_MAC_OS_App/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  First_MAC_OS_App
+//
+//  Created by Kevin OBrien on 4/9/18.
+//  Copyright © 2018 Kevin OBrien. All rights reserved.
+//
+
+import Cocoa
+let g = ""
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ First_MAC_OS_App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  First_MAC_OS_App
+//
+//  Created by Kevin OBrien on 4/9/18.
+//  Copyright © 2018 Kevin OBrien. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
